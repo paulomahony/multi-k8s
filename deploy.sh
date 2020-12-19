59249b76c34f6e558c6c35a13e4971b8980f371d
Paul O'Mahony <paul@codesound.net>
2020-12-19 16:10:07 +0000

    Put deploy.sh back into the original order

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,16 @@
-﻿docker build -t paulom77/multi-server:latest -t paulom77/multi-server:$SHA -f ./server/Dockerfile ./server
-docker push paulom77/multi-server:latest
-docker push paulom77/multi-server:$SHA
-
-docker build -t paulom77/multi-client:latest -t paulom77/multi-client:$SHA -f ./client/Dockerfile ./client
-docker push paulom77/multi-client:latest
-docker push paulom77/multi-client:$SHA
-
+﻿docker build -t paulom77/multi-client:latest -t paulom77/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t paulom77/multi-server:latest -t paulom77/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t paulom77/multi-worker:latest -t paulom77/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push paulom77/multi-client:latest
+docker push paulom77/multi-server:latest
 docker push paulom77/multi-worker:latest
+
+docker push paulom77/multi-client:$SHA
+docker push paulom77/multi-server:$SHA
 docker push paulom77/multi-worker:$SHA
 
 kubectl apply -f k8s
-
-kubectl set image deployments/client-deployment client=paulom77/multi-client:$SHA
 kubectl set image deployments/server-deployment server=paulom77/multi-server:$SHA
+kubectl set image deployments/client-deployment client=paulom77/multi-client:$SHA
 kubectl set image deployments/worker-deployment worker=paulom77/multi-worker:$SHA
-
-
-
-
